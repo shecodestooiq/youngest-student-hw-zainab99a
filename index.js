4d77adc680dc249af9f5b6484f8a7c4924339d3f
zainab99a <amanhal729@gmail.com>
2024-02-12 14:39:17 +0300

    edit code

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function showYoungestStudent(students) { 
-    for (var i = 0; i < students.length; i++) {
-        if(students[i]['age']<25){
-        console.log(`${students[i]['name']} is still youngest` )
-      }
-      else{
-        console.log(`${students[i]['name']} is older` )
-      }
-    
-    }
+  let min =students[0].age
+  for (var i = 0; i < students.length; i++) {
+    let obj=students[i].age;
+    if( obj < min ){
+      min = obj
+      console.log("the youngest student is :",students[i].name,min);
+  }
 }
-  
+}
+
   showYoungestStudent([ 
     {name:"zainab",age:24},
     {name:"yousif",age:28},
     {name:"maryam",age:18},
     {name:"ali",age:25}])
 module.exports = showYoungestStudent;
+
