01fab301f90ed8b6ed47adb053c04dd33357f9a0
zainab99a <amanhal729@gmail.com>
2024-02-12 12:32:52 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function showYoungestStudent(students) { }
-
+function showYoungestStudent(students) { 
+    for (var i = 0; i < students.length; i++) {
+        if(students[i]['age']<25){
+        console.log(`${students[i]['name']} is still youngest` )
+      }
+      else{
+        console.log(`${students[i]['name']} is older` )
+      }
+    
+    }
+}
+  
+  showYoungestStudent([ 
+    {name:"zainab",age:24},
+    {name:"yousif",age:28},
+    {name:"maryam",age:18},
+    {name:"ali",age:25}])
 module.exports = showYoungestStudent;
