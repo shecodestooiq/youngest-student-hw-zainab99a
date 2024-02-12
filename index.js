f596dbceae9718384d0ab26c7b7eb2102261ead2
zainab99a <amanhal729@gmail.com>
2024-02-12 15:33:01 +0300

    edit code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function showYoungestStudent(students) { 
+  //store the first value of array in empty var to compeare
   let min =students[0].age
   for (var i = 0; i < students.length; i++) {
+    //this var take all of array elements to compeare it with min variable
     let obj=students[i].age;
-    if( obj < min ){
+    if( obj <= min ){
       min = obj
       console.log("the youngest student is :",students[i].name,min);
   }
