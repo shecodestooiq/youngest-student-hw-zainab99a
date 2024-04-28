92e03eaa6ef2b994be3f83a3621368357eb15630
zainab99a <amanhal729@gmail.com>
2024-04-28 11:41:08 +0300

    edit code

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 function showYoungestStudent(students) { 
-  if(students.length==0){
+  if(students.length === 0){
     console.log("No students provided");
+    return;
   }
   //store the first value of array in empty var to compeare
-  let min =students[0]
-  for (var i = 0; i < students.length; i++) {
+  let age = students[0].age;
+  let name = students[0].name;
+  for (var i = 1; i < students.length; i++) {
     //this var take all of array elements to compeare it with min variable
-    let obj=students[i].age;
-    if( obj < min ){
-      min = obj
-      console.log("the youngest student is :",students[i].name);
-  }
-}
+    let obj=students[i];
+    if( obj.age < age ){
+      age = obj.age;
+      name=obj.name;
+     
+}}
+console.log("the youngest student is :",name);
+
 }
 
-const userDefinedStudents = [
-  { name: 'Alice', age: 25 },
-  { name: 'Bob', age: 2 },
-  { name: 'Charlie', age: 21 },
-];
-showYoungestStudent(userDefinedStudents);
+
 
 module.exports = showYoungestStudent;
 
